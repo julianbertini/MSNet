caafe3fd08baa4599258b3da2459548cad23ef35
Julian Bertini <julianbertini@gmail.com>
2020-04-16 15:27:57 -0500

    Residual Identity Block done and beginning WNet construction

--- model.py
+++ model.py
@@ -5,110 +5,120 @@ import matplotlib.pyplot as plt
 import os
 import pathlib
 import nibabel as nib
+from viz import Visualize
 
-path_to_labels = "../../Task01_BrainTumour/labelsTr"
-path_to_imgs = "../../Task01_BrainTumour/imagesTr"
+class WNet(tf.keras.Model):
 
-AUTOTUNE = tf.data.experimental.AUTOTUNE
+    def __init__(self):
+        """ Builds the WNet model for whole tumor segmentation
+        """
+        super(WNet, self).__init__(name='')
 
-def read_nifti(img_path_bytes, label=False):
-    """ Reads in an nii.gz format image 
+        self.block_1_a = ResidualIdentityBlock()
 
-        Params:
-            img_path_bytes - bytes, the image path in byte form
-        Returns:
-            img - the image in a tf.float32 nD array (image) or tf.uint8 nD array (label)
-    """
+        self.block_1_b = ResidualIdentityBlock()
 
-    # Decode img_path from bytes to string
-    img_path = img_path_bytes.decode("utf-8")
-    
-    # Load .nii.gz image and convert to tf.float32 dtype
-    img = nib.load(img_path)
-    img = np.asarray(img.dataobj)
 
-    if label:
-        img = tf.image.convert_image_dtype(img, tf.uint8)
-    else:
-        img = tf.image.convert_image_dtype(img, tf.float32)
 
-    return img
+class ResidualIdentityBlock(tf.keras.Model):
 
-def get_label_path(img_path):
-    """ Gets the label image corresponding to the given image path
+    def __init__(self, kernels, n_output_chns, dilation_rates, with_residual=True):
+        """ 
+            Params:
+                kernel - [..., [depth, height, width], ...], 2D array, where the number of rows represents 
+                the number of "sub" blocks in the identity block, and each row is 3 integers denoting the dimensions of the conv. kernel
+               
+                n_output_chns - tuple, with 2 integer values denoting the number of output channels for each conv.
+
+                dilation_rates - [..., [dilation in depth dim, dilation in height dim, dilation in width dim], ...], 2D array,
+                mirroring the kernel array, where it indicates the dilations in depth, height, width, for each kernel.
+        """
+
+        super(ResidualIdentityBlock, self).__init__(name='')
         
-        Params:
-            img_path - tf.Tensor, representing the path to an image file
-        Returns:
-            label_path - tf.Tensor, representing the path to the label file 
-    """
-    parts = tf.strings.split(img_path, os.path.sep)
-    label_path = tf.strings.join([path_to_labels, parts[-1]], os.path.sep)
+        self.kernels = kernels
+        self.dilation_rates = dilation_rates
 
-    return label_path
-
-
-def process_image_train(img_path: tf.Tensor):
-    """ * Callback function for tf.data.Dataset.map to process each image file path.
-        * For each image file path, it returns a corresponding (image, label) pair.
-        * This is the parent function that wraps all other processing helpers.
-        * This is the processing for the training data specifically.
-
-        Params:
-            img_path - tf.Tensor, representing the path to an image file
-        Returns:
-            img, label - tuple of (tf.float32, tf.uint8) arrays representing the image and label arrays
-    """
-
-    label_path = get_label_path(img_path)
-
-    img = read_nifti(img_path.numpy())
-    label = read_nifti(label_path.numpy(), label=True)
-
-    return img, label
-    
-
-def normalize(input_image):
-    """ Normalizes the image data
+        self.strides = [1,1,1]
+        self.reg_decay = 1e-7
+        self.n_output_chns = n_output_chns
+        self.with_residual = with_residual 
         
-        TODO: maybe split this into separate methods for training, testing, etc.
-    """
-    # Cast image to float32 type and normalize to [0,1] pixel values
-    input_image = tf.cast(input_image, tf.float32) / 255.0
+        # 1) 3D convolution layer
+        self.conv3a = tf.keras.layers.Conv3D(
+                    self.n_output_chns, 
+                    self.kernels[0], 
+                    self.strides, 
+                    padding="SAME", 
+                    dilation_rate=self.dilation_rates[0],
+                    use_bias=False,
+                    kernel_regularizer=tf.keras.regularizers.l2(self.reg_decay),
+                    bias_regularizer=tf.keras.regularizers.l2(self.reg_decay),
+                    name="conv3a")
 
-    return input_image, input_mask
+        # 2) Batch normalization layer
+        self.bn3a = tf.keras.layers.BatchNormalization(
+                    beta_regularizer=tf.keras.regularizers.l2(self.reg_decay),
+                    gamma_regularizer=tf.keras.regularizers.l2(self.reg_decay),
+                    name="batch3a")
+            
+        # 3) PReLU activation function
+        self.acti3a = tf.keras.layers.PReLU(name="acti3a")
+        
+        # 4) 3D convolution layer
+        self.conv3b = tf.keras.layers.Conv3D(
+                    self.n_output_chns, 
+                    self.kernels[1], 
+                    self.strides, 
+                    padding="SAME", 
+                    dilation_rate=self.dilation_rates[1],
+                    use_bias=False,
+                    kernel_regularizer=tf.keras.regularizers.l2(self.reg_decay),
+                    bias_regularizer=tf.keras.regularizers.l2(self.reg_decay),
+                    name="conv3b")
 
-#@tf.function
-#def load_image_train(data)
+        # 5) Batch normalization layer
+        self.bn3b = tf.keras.layers.BatchNormalization(
+                    beta_regularizer=tf.keras.regularizers.l2(self.reg_decay),
+                    gamma_regularizer=tf.keras.regularizers.l2(self.reg_decay),
+                    name="batch3b")
+        
+        # 6) PReLU activation function
+        self.acti3b = tf.keras.layers.PReLU(name="acti3b")
+        
+
+
+    def call(self, input_tensor, training=False):
+        
+        # Connect layers
+
+        # 1st sub-block
+        output_tensor = self.conv3a(input_tensor)
+        output_tensor = self.bn3a(output_tensor, training=training)
+        output_tensor = self.acti3a(output_tensor)
+        
+        # 2nd sub-block
+        output_tensor = self.conv3b(output_tensor)
+        output_tensor = self.bn3b(output_tensor, training=training)
+
+        # Add residual inputs  
+        if self.with_residual: 
+            output_tensor += input_tensor
+
+        # Apply final activation
+        output_tensor = self.acti3b(output_tensor)
+
+        return output_tensor
+
 
 def main():
-    
-    img_dir = pathlib.Path(path_to_imgs)
-    
-    img_count = len(list(data_dir.glob("*.nii.gz")))
- 
-    # The tf.data.Dataset API supports writing descriptive and efficient input pipelines.
-    #   - Create a source dataset from your input data
-    #   - Apply dataset transformation to preprocess the data
-    #   - Iterate over the dataset and process the elements
-    # Iteration happens in a streaming fashion, so the full dataset does not need to fit into memory at once. 
-    dataset = tf.data.Dataset
 
-    # Generates a dataset list of all files matching one or more glob patters.
-    # It will return filenames in a non-deterministic random shuffle order.
-    img_ds = dataset.list_files(str(img_dir/"*"))
+    block = ResidualIdentityBlock([[1,3,3], [1,3,3]], 2, [[1,1,1], [1,1,1]])
     
-    
-    # `num_parallel_calls` allows for mltiple images to be loaded/processed in parallel. 
-    # the tf.py_function allows me to convert each element in `img_ds` to numpy format, which is necessary to read nifti images.
-    # we have to do this b/c tensorflow does not have a custom .nii.gz image decoder like jpeg or png.
-    # According to docs, this lowers performance, but I think this is still better than just doing a for loop b/c of the asynchronous
-    train = img_ds.map(lambda x: tf.py_function(func=process_path, inp=[x], Tout=(tf.float32, tf.uint8)) , num_parallel_calls=AUTOTUNE)
-    
-    for image, label in train.take(1):
-        print("Image shape: ", image.numpy().shape)
-        print("Label: ", label.numpy().shape)
-        print(np.nanmin(label.numpy()))
+    _ = block(tf.zeros([1, 10, 10, 10, 1]))
+
+    print(block.layers)
+    print(block.summary())
 
 
 
