fix and test normalization; add complete loss calculation code from niftynet

src/data_preprocessor.py
@@ -10,7 +10,7 @@ from viz import Visualize
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 BATCH_SIZE = 5
-BUFFER_SIZE = 385
+BUFFER_SIZE = 10
 # Right now, the smaller label patch will be centered along the same center point as
 # the image patch. So the label patch will be missing what's left over from the image patch
 # on either side equally.
@@ -171,20 +171,23 @@ class DataPreprocessor():
         #input_image = self.read_nifti(img_path.numpy(), img_channels=4)
         #input_label = self.read_nifti(label_path.numpy(), label=True)
 
-        # Normalize image
-        input_image = self.normalize(input_image)
+        # Make label binary for tumor region in question
+        if self.tumor_region:
+            input_label = tf.where(input_label >= TUMOR_REGIONS[self.tumor_region], tf.constant(1, dtype=tf.uint8), tf.constant(0, dtype=tf.uint8))
 
+        # Fetch random image patch
         image_patch, label_patch = self.get_random_patch(
             input_image, input_label)
+
+
+        # Normalize image patch AFTER creating the patch
+        image_patch = self.normalize(image_patch)
 
         if purpose == "train":
             # Augment Data
             image_patch, label_patch = self.augment_patch(
                 image_patch, label_patch)
 
-        # Make label binary for tumor region in question
-        if self.tumor_region:
-            label_patch = tf.where(label_patch >= TUMOR_REGIONS[self.tumor_region], tf.constant(1, dtype=tf.uint8), tf.constant(0, dtype=tf.uint8))
 
         return image_patch, label_patch
 
@@ -240,21 +243,34 @@ class DataPreprocessor():
             * Loops until a patch is returned where the labels are not all zeros (all background)
 
             Params:
-                input_image - tf.float32 array representing the entireimage 
+                input_image - tf.float32 array representing the entire image 
                 input_label - tf.float32 array representing the entire labeled image
             Returns:
                 image_patch - tf.float32 array representing the image patch 
                 label_path -  tf.float32 array representing the corresponding label patch
         """
-
+        max_tries = 1000 # prevent infinite loop
+        tries = 0
         flag = False
-        while (flag == False):
+        while (flag == False and tries < max_tries):
             image_patch, prev_center = self.__get_image_patch(
                 input_image, IMG_PATCH_SIZE[:-1])
             label_patch, _ = self.__get_image_patch(
                 input_label, LABEL_PATCH_SIZE[:-1], is_label=True, prev_center=prev_center)
-            if tf.math.reduce_sum(label_patch, [0, 1,2]) > 0:
+            tumor_pixels = tf.where(label_patch > 0, tf.ones_like(label_patch, dtype=tf.dtypes.int32), tf.zeros_like(label_patch, dtype=tf.dtypes.int32))
+            tumor_pixels = tf.math.reduce_sum(tumor_pixels)
+            tumor_pixels = tf.cast(tumor_pixels, dtype=tf.float32)
+            total_pixels = tf.size(label_patch, out_type=tf.dtypes.int32)
+            total_pixels = tf.cast(total_pixels, dtype=tf.float32)
+            tumor_frac = tf.math.divide(tumor_pixels, total_pixels)
+            
+            # Keep trying until tumor region is at least 5% of the total patch
+            assert total_pixels > tumor_pixels
+            if tumor_frac > 0.05:
+                print(tumor_frac)
                 flag = True
+
+            tries += 1
 
         return image_patch, label_patch
 
@@ -363,7 +379,7 @@ class DataPreprocessor():
         # we have to do this b/c tensorflow does not have a custom .nii.gz image decoder like jpeg or png.
         # According to docs, this lowers performance, but I think this is still better than just doing a for loop b/c of the asynchronous
         dataset = img_ds.map(lambda img_path: self.map_path_to_patch_pair(
-            img_path, purpose="train"), num_parallel_calls=AUTOTUNE)
+            img_path, purpose="train"), num_parallel_calls=None)
         # dataset = img_ds.map(lambda x: tf.py_function(func=self.process_image_train, inp=[x], Tout=(tf.float32, tf.uint8)),
         #        num_parallel_calls=AUTOTUNE)
 
@@ -401,7 +417,7 @@ class DataPreprocessor():
             * All images should be normalized, including test images
 
             Params:
-                input_image - tf.float32 array representing the image, dims: (240, 240, 155, 4)
+                input_image - tf.float32 array representing the image, dims: (depth, height, width, channels)
 
             TODO: * Maybe split this into separate methods for training, testing, etc.
                   * Also, Matlab model sets all pixels that were 0 before normalization to 0 after normalization.
@@ -410,36 +426,60 @@ class DataPreprocessor():
                   * Matlab code also forces all pixels to be between [-5, 5] (before normalizing to be between [0,1])
                     and this just seems kind of arbitrary. I'm not sure what this does. 
         """
-		print(tf.shape(input_image))
+        
+        # Normalize across each z-slice and each modality (as they do in Coursera)
+        c_stacks = [] 
+        for c in range(input_image.shape[3]):
+          z_stacks = []
+          for z in range(input_image.shape[0]):
+          
+            image_slice = input_image[z,:,:,c]
+            centered = image_slice - tf.math.reduce_mean(image_slice)  
+            
+            std = tf.math.reduce_std(centered)
+            if std != 0:
+              centered_scaled = centered / std
+              z_stacks.append(centered_scaled)
+            else:
+              z_stacks.append(centered)
+          
+          c_stacks.append(tf.stack(z_stacks, axis=0))
+        norm_image = tf.stack(c_stacks, axis=-1)
+      
+        #print(tf.math.reduce_std(norm_image[50,:,:,2]))
+  
+        #input_image = tf.transpose(input_image, perm=(2, 1, 0, 3))  
 
-        # Calculate the mean for the image for each modality
-        # mean is an array with 4 elements: the mean for each modality (or "sequence")
-        mean = tf.math.reduce_mean(input_image, [0, 1])
-        # Same for standard deviation
-        std = tf.math.reduce_std(input_image, [0, 1])
+        ## Calculate the mean for the image for each modality
+        ## mean is an array with 4 elements: the mean for each modality (or "sequence")
+        #mean = tf.math.reduce_mean(input_image, [0, 1])
+        ## Same for standard deviation
+        #std = tf.math.reduce_std(input_image, [0, 1])
 
-        # Subtract the mean from each element and divide by standard deviation
-        input_image = tf.math.subtract(input_image, mean)
-		input_image = tf.where(std != 0, tf.divide(input_image,std), input_image)
+        ## Subtract the mean from each element and divide by standard deviation
+        #input_image = tf.math.subtract(input_image, mean)
+        #input_image = tf.where(std != 0, tf.divide(input_image,std), input_image)
 
-        # Set image values to range from [0,1]
+        ## Set image values to range from [0,1]
 
-        ## Add min to make all elements >= 0
-        #min_per_mod = tf.math.reduce_min(input_image, [0, 1,2])
-        ## subtract since min is negative, and we want to add
-        #input_image = tf.math.subtract(input_image, min_per_mod)
-        ## Divide by max to make all elements <= 1
-        #max_per_mod = tf.math.reduce_max(input_image, [0, 1,2])
-        #input_image = tf.math.divide(input_image, max_per_mod)
+        ### Add min to make all elements >= 0
+        ##min_per_mod = tf.math.reduce_min(input_image, [0, 1,2])
+        ### subtract since min is negative, and we want to add
+        ##input_image = tf.math.subtract(input_image, min_per_mod)
+        ### Divide by max to make all elements <= 1
+        ##max_per_mod = tf.math.reduce_max(input_image, [0, 1,2])
+        ##input_image = tf.math.divide(input_image, max_per_mod)
 
-		return input_image
+        #input_image = tf.transpose(input_image, perm=(2, 1, 0, 3))
+
+        return norm_image
 
 
 def main():
     """ Use for testing/debugging purposes
     """
 
-    dp = DataPreprocessor()
+    dp = DataPreprocessor(tumor_region="whole tumor")
 
     img_dir = pathlib.Path(dp.path_to_train_imgs)
 
@@ -455,7 +495,7 @@ def main():
     img_ds = dataset.list_files(str(img_dir/"*"))
 
     # Create batches, shuffle, etc
-    train = dp.prepare_for_training(img_ds, "train")
+    train = dp.prepare_for_training(img_ds)
 
     # Visualizing 3D volumes
     viz = Visualize()
@@ -466,6 +506,14 @@ def main():
           #loss = gdl(label, label)
           #print("dice coeff")
           #print((loss-1)*-1)
+          # [batch, depth, chn]
+          #print(image.shape)
+          for z in range(image.shape[1]):
+            img_slice = image[3,z,:,:,2]
+            std = tf.math.reduce_std(image)
+            mean = tf.math.reduce_mean(image)
+            #print(std)
+            #print(mean)
 
           viz.multi_slice_viewer([image.numpy()[0, :,:,:, 0], label.numpy()[0,:,:,:, 0]])
           plt.show()

src/train.py
@@ -27,72 +27,137 @@ class DiceScore(tf.keras.metrics.Metric):
         self.score = self.add_weight(name='score', initializer='zeros')
 
     def update_state(self, y_true, y_pred, sample_weight=None):
+        """
+          Calculate the average dice score across the current training batch
+        """ 
+        # for each volume in batch
+        n_scores = 0
+        score = 0
+        for b in range(tf.shape(y_pred)[0]):
+          vol_pred = tf.cast(y_pred[b], dtype=tf.float32)
+          vol_true = tf.cast(y_true[b], dtype=tf.float32)
+          
+          vol_pred = tf.nn.softmax(vol_pred)
+          vol_pred = tf.argmax(vol_pred, axis=-1)
+          vol_pred = vol_pred[..., tf.newaxis]
 
-		dsc_numerator = 2 * tf.reduce_sum(tf.multiply(y_pred,y_true)) + 1e-10
-		dsc_denominator = tf.reduce_sum(y_pred) + tf.reduce_sum(y_true) + 1e-10
+          dsc_numerator = 2 * tf.reduce_sum(tf.multiply(vol_pred, vol_true)) + 1e-10
+          dsc_denominator = tf.reduce_sum(vol_pred) + tf.reduce_sum(vol_true) + 1e-10
+
+          score = dsc_numerator / dsc_denominator  
+          score += score
+          n_scores += 1
 		
-        self.score = dsc_numerator / dsc_denominator 
+        self.score = score / n_scores
 
     def result(self):
         return self.score
 
 class GeneralizedDiceLoss(tf.keras.losses.Loss):
 
-    def labels_to_one_hot(self, ground_truth, num_classes=1):
-        """
-    Converts ground truth labels to one-hot, sparse tensors.
-    Used extensively in segmentation losses.
-
-    :param ground_truth: ground truth categorical labels (rank `N`)
-    :param num_classes: A scalar defining the depth of the one hot dimension
-        (see `depth` of `tf.one_hot`)
-    :return: one-hot sparse tf tensor
-        (rank `N+1`; new axis appended at the end)
-    """
-    # read input/output shapes
-        if isinstance(num_classes, tf.Tensor):
-            num_classes_tf = tf.cast(num_classes, dtype=tf.int32)
-        else:
-            num_classes_tf = tf.constant(num_classes, tf.int32)
-        input_shape = tf.shape(ground_truth)
-        output_shape = tf.concat(
-            [input_shape, tf.reshape(num_classes_tf, (1,))], 0)
-
-        if num_classes == 1:
-            # need a sparse representation?
-            return tf.reshape(ground_truth, output_shape)
-
-        # squeeze the spatial shape
-        ground_truth = tf.reshape(ground_truth, (-1,))
-        # shape of squeezed output
-        dense_shape = tf.stack([tf.shape(ground_truth)[0], num_classes_tf], 0)
-
-        # create a rank-2 sparse tensor
-        ground_truth = tf.cast(ground_truth, dtype=tf.int64)
-        ids = tf.range(tf.cast(dense_shape[0], dtype=tf.int64), dtype=tf.int64)
-        ids = tf.stack([ids, ground_truth], axis=1)
-        one_hot = tf.sparse.SparseTensor(
-            indices=ids,
-            values=tf.ones_like(ground_truth, dtype=tf.float32),
-            dense_shape=tf.cast(dense_shape, dtype=tf.int64))
-
-        # resume the spatial dims
-        one_hot = tf.sparse.reshape(one_hot, output_shape)
-
-        return one_hot
-
     def call(self, ground_truth, prediction, weight_map=None):
         """
-                `weight_map` represents same thing as `loss_weight` in tf
-                except that we apply it here directly instead of passing in
-                through the model.fit attribute `loss_weight`
+        Compute loss from `prediction` and `ground truth`,
+        the computed loss map are weighted by `weight_map`.
+
+        if `prediction `is list of tensors, each element of the list
+        will be compared against `ground_truth` and the weighted by
+        `weight_map`. (Assuming the same gt and weight across scales)
+
+        :param prediction: input will be reshaped into
+            ``(batch_size, N_voxels, num_classes)``
+        :param ground_truth: input will be reshaped into
+            ``(batch_size, N_voxels, ...)``
+        :param weight_map: input will be reshaped into
+            ``(batch_size, N_voxels, ...)``
+        :return:
+        """
+        with_softmax = True
+        num_classes = tf.shape(prediction)[-1]
+        with tf.device('/cpu:0'):
+
+            # prediction should be a list for multi-scale losses
+            # single scale ``prediction`` is converted to ``[prediction]``
+            if not isinstance(prediction, (list, tuple)):
+                prediction = [prediction]
+
+            data_loss = []
+            for ind, pred in enumerate(prediction):
+
+                # go through each scale
+                def _batch_i_loss(*args):
+                    """
+                    loss for the `b_id`-th batch (over spatial dimensions)
+
+                    :param b_id:
+                    :return:
+                    """
+                    # unpacking input from map_fn elements
+                    if len(args[0]) == 2:
+                        # pred and ground_truth
+                        pred_b, ground_truth_b = args[0]
+                        weight_b = None
+                    else:
+                        pred_b, ground_truth_b, weight_b = args[0]
+
+                    pred_b = tf.reshape(pred_b, [-1, num_classes])
+                    # performs softmax if required
+                    if with_softmax:
+                        pred_b = tf.cast(pred_b, dtype=tf.float32)
+                        pred_b = tf.nn.softmax(pred_b)
+
+                    # reshape pred, ground_truth, weight_map to the same
+                    # size: (n_voxels, num_classes)
+                    # if the ground_truth has only one channel, the shape
+                    # becomes: (n_voxels,)
+                    if not pred_b.shape.is_fully_defined():
+                        ref_shape = tf.stack(
+                            [tf.shape(pred_b)[0], tf.constant(-1)], 0)
+                    else:
+                        ref_shape = pred_b.shape.as_list()[:-1] + [-1]
+
+                    ground_truth_b = tf.reshape(ground_truth_b, ref_shape)
+                    if ground_truth_b.shape.as_list()[-1] == 1:
+                        ground_truth_b = tf.squeeze(ground_truth_b, axis=-1)
+
+                    if weight_b is not None:
+                        weight_b = tf.reshape(weight_b, ref_shape)
+                        if weight_b.shape.as_list()[-1] == 1:
+                            weight_b = tf.squeeze(weight_b, axis=-1)
+
+                    # preparing loss function parameters
+                    loss_params = {
+                        'prediction': pred_b,
+                        'ground_truth': ground_truth_b,
+                        'weight_map': weight_b}
+
+                    return tf.cast(self.generalised_dice_loss(**loss_params), dtype=tf.float32)
+
+                if weight_map is not None:
+                    elements = (pred, ground_truth, weight_map)
+                else:
+                    elements = (pred, ground_truth)
+
+                loss_batch = tf.map_fn(
+                    fn=_batch_i_loss,
+                    elems=elements,
+                    dtype=tf.float32,
+                    parallel_iterations=1)
+
+                # loss averaged over batch
+                data_loss.append(tf.math.reduce_mean(loss_batch))
+            # loss averaged over multiple scales
+            return tf.math.reduce_mean(data_loss)
+
+    def generalised_dice_loss(self, prediction, ground_truth, weight_map=None):
+        """
+        `weight_map` represents same thing as `loss_weight` in tf
+        except that we apply it here directly instead of passing in
+        through the model.fit attribute `loss_weight`
         """
         type_weight = "Square"
 
         prediction = tf.cast(prediction, tf.float32)
-        # Calculate softmax for prediction. Default is to reduce based on last
-        # dimension.
-        prediction = tf.nn.softmax(prediction)
 
         if len(ground_truth.shape) == len(prediction.shape):
             ground_truth = ground_truth[..., -1]
@@ -138,6 +203,48 @@ class GeneralizedDiceLoss(tf.keras.losses.Loss):
                                           generalised_dice_score)
         return 1 - generalised_dice_score
 
+    def labels_to_one_hot(self, ground_truth, num_classes=1):
+        """
+        Converts ground truth labels to one-hot, sparse tensors.
+        Used extensively in segmentation losses.
+
+        :param ground_truth: ground truth categorical labels (rank `N`)
+        :param num_classes: A scalar defining the depth of the one hot dimension
+            (see `depth` of `tf.one_hot`)
+        :return: one-hot sparse tf tensor
+            (rank `N+1`; new axis appended at the end)
+        """
+        # read input/output shapes
+        if isinstance(num_classes, tf.Tensor):
+            num_classes_tf = tf.cast(num_classes, dtype=tf.int32)
+        else:
+            num_classes_tf = tf.constant(num_classes, tf.int32)
+        input_shape = tf.shape(ground_truth)
+        output_shape = tf.concat(
+            [input_shape, tf.reshape(num_classes_tf, (1,))], 0)
+
+        if num_classes == 1:
+            # need a sparse representation?
+            return tf.reshape(ground_truth, output_shape)
+
+        # squeeze the spatial shape
+        ground_truth = tf.reshape(ground_truth, (-1,))
+        # shape of squeezed output
+        dense_shape = tf.stack([tf.shape(ground_truth)[0], num_classes_tf], 0)
+
+        # create a rank-2 sparse tensor
+        ground_truth = tf.cast(ground_truth, dtype=tf.int64)
+        ids = tf.range(tf.cast(dense_shape[0], dtype=tf.int64), dtype=tf.int64)
+        ids = tf.stack([ids, ground_truth], axis=1)
+        one_hot = tf.sparse.SparseTensor(
+            indices=ids,
+            values=tf.ones_like(ground_truth, dtype=tf.float32),
+            dense_shape=tf.cast(dense_shape, dtype=tf.int64))
+
+        # resume the spatial dims
+        one_hot = tf.sparse.reshape(one_hot, output_shape)
+
+        return one_hot
 
 def main():
 