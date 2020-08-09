fix dice score metric; fix weight_map calculation

src/data_preprocessor.py
@@ -9,7 +9,7 @@ from viz import Visualize
 #from train import *
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-BATCH_SIZE = 5
+BATCH_SIZE = 4
 BUFFER_SIZE = 10
 # Right now, the smaller label patch will be centered along the same center point as
 # the image patch. So the label patch will be missing what's left over from the image patch
@@ -53,7 +53,7 @@ class DataPreprocessor():
       image and label passed in should be PRE normalization, so that 
       the background is still zero.
       """
-      weight_map = tf.where(image_patch > tf.constant(0, dtype=tf.float32), tf.ones_like(label_patch, dtype=tf.dtypes.int32), tf.zeros_like(label_patch, dtype=tf.dtypes.int32))
+      weight_map = tf.where(image_patch > tf.constant(0, dtype=tf.float32), tf.ones_like(image_patch, dtype=tf.dtypes.int32), tf.zeros_like(image_patch, dtype=tf.dtypes.int32))
       
       return weight_map
 
@@ -515,12 +515,6 @@ def main():
   
     for i in range(10):
       for image, label in train.take(1):
-          #gdl = GeneralizedDiceLoss()
-          #loss = gdl(label, label)
-          #print("dice coeff")
-          #print((loss-1)*-1)
-          # [batch, depth, chn]
-          #print(image.shape)
           for z in range(image.shape[1]):
             img_slice = image[3,z,:,:,2]
             std = tf.math.reduce_std(image)

src/eval.py
@@ -10,29 +10,14 @@ from data_preprocessor import *
 from model import *
 
 
-def __tp_map(seg, ref):
-	"""
-		This function calculates the true positive map (i.e. how many 
-		reference voxels are positive)
-
-		:return: TP map
-	"""
-	seg = tf.cast(seg, dtype=bool)
-	ref = tf.cast(ref, dtype=bool)
-	return tf.cast(tf.math.logical_and(ref, seg), dtype=tf.float32)
-
-def __tp(tp_map):
-	return tf.math.reduce_sum(tp_map)
-
 def dice_score(seg, ref):
-	tp_map = __tp_map(seg, ref)
-	tp = __tp(tp_map)
-	seg = tf.cast(seg, dtype=tf.float32)
-	ref = tf.cast(ref, dtype=tf.float32)	
-	numerator = tf.cast(tf.math.multiply(tf.constant(2, dtype=tf.float32), tp), dtype=tf.float32)
-	denominator = tf.math.reduce_sum(tf.math.add(ref, seg))
+  seg = tf.cast(seg, dtype=tf.float32)
+  ref = tf.cast(ref, dtype=tf.float32)
 
-	return tf.math.divide(numerator, denominator)
+  dsc_numerator = tf.constant(2, dtype=tf.float32) * tf.reduce_sum(tf.multiply(seg, ref)) + 1e-10
+  dsc_denominator = tf.reduce_sum(seg) + tf.reduce_sum(ref) + 1e-10
+
+  return tf.math.divide(dsc_numerator, dsc_denominator)
 
 def dice_similarity(model, dataset=None, num=60):
 	scores = []

src/train.py
@@ -8,6 +8,7 @@ import nibabel as nib
 from viz import Visualize
 from data_preprocessor import *
 from model import *
+from eval import dice_score
 
 # I'm actually guessing at this order... I think it's right
 MODALITIES = {"t1": 0, "t1c": 1, "t2": 2, "flair": 3}
@@ -34,26 +35,22 @@ class DiceScore(tf.keras.metrics.Metric):
         """ 
         # for each volume in batch
         n_scores = 0.0
-        score = 0.0
+        sum_score = 0.0
+        current_score = 0.0
         for b in range(tf.shape(y_pred)[0]):
-          #print('y_pred in dice score:', y_pred.shape)
-          vol_pred = y_pred[b]
           vol_true = tf.cast(y_true[b], dtype=tf.float32)
           
+          vol_pred = y_pred[b]
           vol_pred = tf.nn.softmax(vol_pred)
           vol_pred = tf.argmax(vol_pred, axis=-1)
-          vol_pred = vol_pred[..., tf.newaxis]
+          vol_pred = tf.expand_dims(vol_pred, axis=-1)
           vol_pred = tf.cast(vol_pred, tf.float32)
-          #print(vol_pred)
-
-          dsc_numerator = 2 * tf.reduce_sum(tf.multiply(vol_pred, vol_true)) + 1e-10
-          dsc_denominator = tf.reduce_sum(vol_pred) + tf.reduce_sum(vol_true) + 1e-10
-
-          score = dsc_numerator / dsc_denominator  
-          score += score
+          
+          current_score = dice_score(vol_pred, vol_true)
+          sum_score += current_score
           n_scores += 1
 		
-        self.score = score / n_scores
+        self.score = sum_score / n_scores
 
     def result(self):
         return self.score